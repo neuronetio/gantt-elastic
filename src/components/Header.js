6ceed1896092b889ab58147485b319029dd75b1e
neuronet <neuronet.it@gmail.com>
2018-07-11 22:27:49 +0200

    bootstrap removal, vue from local env

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ export function Header(prefix, self) {
   return self.wrapComponent({
 
     template: `<div class="elastigantt__header">
-          <input type="range" v-model="scale" max="22" min="1">
+          <input type="range" v-model="scale" max="24" min="1">
           <input type="range" v-model="height" max="100" min="6">
       </div>`,
 
