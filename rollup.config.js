62586de904072a046c8faecca7af7dfe053112db
neuronetio <neuronet.it@gmail.com>
2018-09-05 23:42:33 +0200

    source of truth

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ import resolve from 'rollup-plugin-node-resolve';
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-  input: 'src/sourceOfTruth.js',
+  input: 'src/index.js',
   output: {
     file: 'dist/elastigantt.js',
     format: 'iife', // immediately-invoked function expression — suitable for <script> tags
