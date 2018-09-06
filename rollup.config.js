7c364eb6d16c5b3a5414d77e3ff388f6ff9315fd
neuronetio <neuronet.it@gmail.com>
2018-09-06 21:26:40 +0200

    rollup vue plugin installed

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import resolve from 'rollup-plugin-node-resolve';
+import VuePlugin from 'rollup-plugin-vue'
 // import commonjs from 'rollup-plugin-commonjs';
 // import uglify from 'rollup-plugin-uglify-es';
 
@@ -16,6 +17,7 @@ export default {
   },
   plugins: [
     resolve(), // tells Rollup how to find date-fns in node_modules
+    VuePlugin(),
     // commonjs(),  converts date-fns to ES modules
     // production && uglify()  minify, but only in production
   ],
