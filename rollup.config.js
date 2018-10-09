eb7ccc0e31db3378a4e1dbe4e55b4ecc4ee112e4
neuronetio <neuronet.it@gmail.com>
2018-10-09 22:05:04 +0200

    rollup config and minified versions

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import VuePlugin from 'rollup-plugin-vue'
 import resolve from 'rollup-plugin-node-resolve';
-// import commonjs from 'rollup-plugin-commonjs';
-// import uglify from 'rollup-plugin-uglify-es';
+//import commonjs from 'rollup-plugin-commonjs';
+import minify from 'rollup-plugin-minify-es';
 
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
@@ -25,7 +25,21 @@ export default[
       include: 'src/**'
     }
   }, {
-    input : 'src/standalone.js',
+    input: 'src/index.js',
+    output: {
+      file: 'dist/elastigantt.min.js',
+      format: 'iife', // immediately-invoked function expression — suitable for <script> tags
+      sourcemap: false,
+      name: 'Elastigantt'
+    },
+    plugins: [
+      VuePlugin(), resolve(), minify()
+    ],
+    watch: {
+      include: 'src/**'
+    }
+  }, {
+    input: 'src/standalone.js',
     output: {
       file: 'dist/elastigantt.standalone.js',
       format: 'iife', // immediately-invoked function expression — suitable for <script> tags
@@ -36,6 +50,20 @@ export default[
       VuePlugin(), resolve(), // tells Rollup how to find date-fns in node_modules
       // commonjs(),  converts date-fns to ES modules
       // production && uglify()  minify, but only in production
+    ],
+    watch: {
+      include: 'src/**'
+    }
+  }, {
+    input: 'src/standalone.js',
+    output: {
+      file: 'dist/elastigantt.standalone.min.js',
+      format: 'iife', // immediately-invoked function expression — suitable for <script> tags
+      sourcemap: false,
+      name: 'Elastigantt'
+    },
+    plugins: [
+      VuePlugin(), resolve(), minify()
     ],
     watch: {
       include: 'src/**'
