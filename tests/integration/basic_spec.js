ac6094904fd34149e7aafd083e6fcb106ddf163c
neuronet.io <neuronet.io@gmail.com>
2019-04-12 21:41:29 +0200

    locale name instead of code, test update, examples update

--- tests/integration/basic_spec.js
+++ tests/integration/basic_spec.js
@@ -30,18 +30,49 @@ function expectTimeZoomChange(url) {
   });
 }
 
+function expectAddTask(url) {
+  cy.visit(url, { timeout: 10000 }).then(window => {
+    const gantt = window.app.$children[0];
+    expect(gantt.state.tasks.length).to.equal(15);
+    expect(typeof gantt.getTask).to.equal('function');
+    const task = gantt.getTask(7);
+    expect(task.children.length).to.equal(1);
+    expect(task.allChildren.length).to.equal(2);
+    expect(task.children[0]).to.equal(8);
+    cy.get('#btn-add')
+      .click()
+      .wait(1000)
+      .then(() => {
+        const task = gantt.getTask(7);
+        expect(task.children.length).to.equal(2);
+        expect(task.allChildren.length).to.equal(3);
+        expect(task.children[1]).to.equal(88);
+      });
+  });
+}
+
+const umd = '/tests/assets/umd.html';
+const standalone = '/tests/assets/standalone.html';
+
 describe('Basic', () => {
   it('should mount gantt-elastic - umd', () => {
-    expectIsMounted('/tests/assets/umd.html');
+    expectIsMounted(umd);
   });
   it('should mount gantt-elastic - standalone', () => {
-    expectIsMounted('/tests/assets/standalone.html');
+    expectIsMounted(standalone);
   });
 
   it('should change time-zoom - umd', () => {
-    expectTimeZoomChange('/tests/assets/umd.html');
+    expectTimeZoomChange(umd);
   });
   it('should change time-zoom - standalone', () => {
-    expectTimeZoomChange('/tests/assets/standalone.html');
+    expectTimeZoomChange(standalone);
+  });
+
+  it('should add task - umd', () => {
+    expectAddTask(umd);
+  });
+  it('should add task - standalone', () => {
+    expectAddTask(standalone);
   });
 });
